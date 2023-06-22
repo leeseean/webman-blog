6384296e74df391724ef3b15886bac1775539c68
leeseean <leeseean@foxmail.com>
2023-06-22 19:00:14 +0800

    webman_admin

--- plugin/admin/config/thinkorm.php
+++ plugin/admin/config/thinkorm.php
@@ -10,11 +10,11 @@ return [
             // 数据库名
             'database' => 'webman_admin',
             // 数据库用户名
-            'username' => 'root',
+            'username' => 'webman_admin',
             // 数据库密码
             'password' => '123456',
             // 数据库连接端口
-            'hostport' => 3308,
+            'hostport' => 3306,
             // 数据库连接参数
             'params' => [
                     // 连接超时3秒
