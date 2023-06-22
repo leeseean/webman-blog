fda8bda0cec6470e435dd894e3fb28f2309c009f
leeseean <leeseean@foxmail.com>
2023-06-22 19:53:02 +0800

    docker stop app

--- stopapp.sh
+++ stopapp.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker stop app
+OUTPUT=$(docker ps -f ancestor=developer/app:1.0 --format "{{.ID}}")
+echo $OUTPUT
+docker stop $OUTPUT
+docker rm $OUTPUT
 docker rmi developer/app:1.0
