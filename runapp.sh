a2c9aa42647746e828fced2487ea045729e7b98f
leeseean <leeseean@foxmail.com>
2023-06-23 09:20:20 +0800

    ip addr show eth0 | awk '/inet / {print $2}' | cut -d '/' -f 1

--- runapp.sh
+++ runapp.sh
@@ -4,7 +4,7 @@
 docker build --rm -t developer/app:1.0 .
 
 # Run Docker container
-# docker run -d --restart=always --name=app -p 8787:8787 -e HOST_IP=$(ip route | awk '/default/ { print $3 }') developer/app:1.0
+# docker run -d --restart=always --name=app -p 8787:8787 -e HOST_IP=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d '/' -f 1) developer/app:1.0
 # Run Docker container 
-# -e HOST_IP=$(ip route | awk '/default/ { print $3 }') 这句是将宿主机的 IP 地址作为 HOST_IP 环境变量传递给容器
- docker run -d --restart=always --name=app -p 8787:8787 -e HOST_IP=$(ip route | awk '/default/ { print $3 }') developer/app:1.0
+# -e HOST_IP=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d '/' -f 1) 这句是将宿主机的 IP 地址作为 HOST_IP 环境变量传递给容器
+ docker run -d --restart=always --name=app -p 8787:8787 -e HOST_IP=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d '/' -f 1) developer/app:1.0
