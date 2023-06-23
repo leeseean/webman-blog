5625978ea7b8adc4892610aafc68118529e1132a
leeseean <leeseean@foxmail.com>
2023-06-23 08:31:42 +0800

    # -e HOST_IP=$(ip route | awk '/default/ { print $3 }') 这句是将宿主机的 IP 地址作为 HOST_IP 环境变量传递给容器

--- runapp.sh
+++ runapp.sh
@@ -4,4 +4,7 @@
 docker build --rm -t developer/app:1.0 .
 
 # Run Docker container
-docker run -d --restart=always --name=app -p 8787:8787 developer/app:1.0
+# docker run -d --restart=always --name=app -p 8787:8787 -e HOST_IP=$(ip route | awk '/default/ { print $3 }') developer/app:1.0
+# Run Docker container 
+# -e HOST_IP=$(ip route | awk '/default/ { print $3 }') 这句是将宿主机的 IP 地址作为 HOST_IP 环境变量传递给容器
+ docker run -d --restart=always --name=app -p 8787:8787 -e HOST_IP=$(ip route | awk '/default/ { print $3 }') developer/app:1.0
